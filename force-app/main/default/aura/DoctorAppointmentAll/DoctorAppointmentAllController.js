f154d118e5c38edb1cafebc8967b5bd7b17e842a
unknown <cliu.jonathan@gmail.com>
2019-12-27 11:39:34 -0500

    Added doctor appointment view

--- force-app/main/default/aura/DoctorAppointmentAll/DoctorAppointmentAllController.js
+++ force-app/main/default/aura/DoctorAppointmentAll/DoctorAppointmentAllController.js
@@ -1,15 +1,48 @@
 ({
-	init : function(component, event, helper) {
-		helper.getFilters(component, "v.FilterOptions");
+    init : function(component, event, helper) {
+        helper.getFilters(component, "v.FilterOptions");
         
         component.set("v.AppointmentsColumns", 
-        [{ label: 'Start time', fieldName: 'Start_Time__c', Type: 'DateTime'},
-          {label: 'End time', fieldName: 'End_Time__c', Type:'Datetime'},
-         {label: 'Status', fieldName: 'Status__c', Type:'String'}]);
+                      [{label:'Patient Name', fieldName:'Customer_Name', type: 'string', sortable:true},
+                       {label:'Appointment Description',fieldName:'Description__c', type: 'string'},
+                       {label:'Language Preference', fieldName:'Language_Preference__c', type: 'string', sortable:true},
+                       {label:'Your Message', fieldName:'Doctor_Message__c',type:'string'},
+                       {label:'Appointment Office', fieldName:'Office_Name',type: 'string', sortable:true},     
+                       {label: 'Start time', fieldName: 'Start_Time__c', type: 'date', typeAttributes:{
+                           year: "2-digit",
+                           month: "numeric",
+                           day: "2-digit",
+                           hour: "2-digit",
+                           minute: "2-digit",
+                           hour12: true
+                       }, sortable:true},
+                       {label: 'End time', fieldName: 'End_Time__c', type:'date', typeAttributes:{
+                           year: "2-digit",
+                           month: "numeric",
+                           day: "2-digit",
+                           hour: "2-digit",
+                           minute: "2-digit",
+                           hour12: true
+                       }, sortable:true},
+                       {label: 'Status', fieldName: 'Status__c', Type:'String', sortable:true},
+                       {label: 'Phone Number', fieldName:'Customer_Phone', Type:'phone', sortable:true}
+                      ]);
+        component.find('filterselect').set('v.value','All');
+        helper.updateAppointments(component, "filterselect", "v.AppointmentsList");
+    },
+    updateAppointments: function(component,event,helper){
         
         helper.updateAppointments(component, "filterselect", "v.AppointmentsList");
-	},
-    updateAppointments: function(component,event,helper){
+    },
+    refreshAppointments: function(component,event,helper){
+        component.set('v.AppointmentsList',null);
         helper.updateAppointments(component, "filterselect", "v.AppointmentsList");
+    },
+    updateSort: function(component,event,helper){
+        let field=event.getParam('fieldName');
+        let direction=event.getParam('sortDirection');
+        component.set("v.SortCategory",field);
+        component.set("v.SortDirection",direction);
+        helper.sortData(component,field,direction, "v.AppointmentsList");
     }
 })
--- force-app/main/default/aura/DoctorAppointmentAll/DoctorAppointmentAllHelper.js
+++ force-app/main/default/aura/DoctorAppointmentAll/DoctorAppointmentAllHelper.js
@@ -1,5 +1,5 @@
 ({
-	getFilters : function(component, attribute) {
+    getFilters : function(component, attribute) {
         let func= component.get('c.getAppointmentFilters');
         func.setCallback(this,function(response){
             if(response.getState()=="SUCCESS"){
@@ -8,15 +8,42 @@
         });
         $A.enqueueAction(func);
         
-	},
+    },
     updateAppointments : function (component, filter, list){
-            let func=component.get('c.getAppointmentsByStatus');
+        let func=component.get('c.getAppointmentsByStatus');
         func.setParams({'Filter':component.find(filter).get('v.value')});
         func.setCallback(this,function(response){
             if(response.getState()=="SUCCESS"){
-                component.set(list,response.getReturnValue());
+                let responseData=response.getReturnValue();
+                
+                //copy fields to base level for lightning datatable
+                for(let i=0;i<responseData.length;i++){
+                    responseData[i].Customer_Name=responseData[i].Customer__r.Name;
+                    responseData[i].Customer_Phone=responseData[i].Customer__r.Phone;
+                    responseData[i].Office_Name=responseData[i].Residence__r.Office_Location__r.Name;
+                }
+                console.log(responseData);
+                component.set(list,responseData);
+                
             }
         });
         $A.enqueueAction(func);
-}
+    },
+    sortData : function(component, field, direction, list){
+        let table=component.get(list);
+        let reverseDirection = direction !== 'asc';
+        table.sort(this.sortBy(field,reverseDirection));
+        component.set(list,table);
+    },
+    //function by developer.salesforce.com
+    sortBy: function (field, reverse, primer) {
+        var key = primer ?
+            function(x) {return primer(x[field])} :
+        function(x) {return x[field]};
+        //checks if the two rows should switch places
+        reverse = !reverse ? 1 : -1;
+        return function (a, b) {
+            return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+        }
+    }
 })
